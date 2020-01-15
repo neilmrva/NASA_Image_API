fde4455f71f25caa74f9c8c86b7a1323509245c9
Neil Mrva <neil.mrva@gmail.com>
2020-01-14 23:17:24 -0500

    - Deleted scene delegate class and entries in Xcode project to support testing in iOS 12.4 on older device - Updated AppDelegate with dependency injection code to supply initial view controller with image model controller

--- NASA Image API/AppDelegate.swift
+++ NASA Image API/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  NASA Image API
 //
-//  Created by Development on 1/12/20.
+//  Created by Neil Mrva on 1/12/20.
 //  Copyright © 2020 Neil Mrva. All rights reserved.
 //
 
@@ -11,35 +11,49 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate
 {
-    // This will get assigned when app starts (iOS 12) - Doesn't seem to work in xcode 11
-    //var window:UIWindow?
+    // This will get assigned when app starts (iOS 12) on device testing - Xcode 11 uses scenedelegate when using the simulator.
+    // Had to delete scenedelegate and it's info.plist entry to force window variable to be set here
+    var window:UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
     {
-        // Override point for customization after application launch.
-        
-        // Inject dependecy of the NASAImageDataModelController into the initial view controller
-//        let initialController = window?.rootViewController as! UINavigationController
-//        let imageCollectionViewController = initialController.viewControllers.first as! ImageCollectionViewController
-//        imageCollectionViewController.nasaImageDataModelController = NASAImageDataModelController()
-        
+        // Inject NASAImageDataModelController dependency into images view controller
+        if let initialViewController = window?.rootViewController as? UINavigationController
+        {
+            if let imageCollectionViewController = initialViewController.viewControllers.first as? ImageCollectionViewController
+            {
+                imageCollectionViewController.nasaImageDataModelController = NASAImageDataModelController()
+            }
+        }
+            
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
-
-    @available(iOS 13.0, *)
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    func applicationWillResignActive(_ application: UIApplication)
+    {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
-    @available(iOS 13.0, *)
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    func applicationDidEnterBackground(_ application: UIApplication)
+    {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication)
+    {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication)
+    {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication)
+    {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
 }
